a31ba66e75bab2d0a1e08a283a2c1959e69ac317
Nacho <nacho987600@gmail.com>
2025-03-27 17:02:47 -0300

    Finished detailed fish UI

--- fish_index_ui.gd
+++ fish_index_ui.gd
@@ -32,6 +32,16 @@ var is_fish_index_open: bool = false
 @onready var shiny_fish_frame = $NinePatchRect/GridContainer/ShinyFishFrame
 @onready var striped_tigerbarb_fish_frame = $NinePatchRect/GridContainer/StripedTigerbarbFishFrame
 
+@onready var detailed_fish_sprite = $NinePatchRect/DetailedFishInfoPanel/NinePatchRect/DetailedFishSprite
+@onready var detailed_fish_name = $NinePatchRect/DetailedFishInfoPanel/NinePatchRect/VBoxContainer/VBoxContainer/DetailedFishName
+@onready var detailed_fish_rarity = $NinePatchRect/DetailedFishInfoPanel/NinePatchRect/VBoxContainer/VBoxContainer/DetailedFishRarity
+@onready var detailed_fish_description = $NinePatchRect/DetailedFishInfoPanel/NinePatchRect/VBoxContainer/DetailedFishDescription
+@onready var detailed_fish_value = $NinePatchRect/DetailedFishInfoPanel/NinePatchRect/VBoxContainer/DetailedFishValue/Label
+
+@onready var detailed_fish_info_panel = $NinePatchRect/DetailedFishInfoPanel
+@onready var no_fish_selected_text = $NinePatchRect/NoFishSelectedText
+
+
 func _process(delta):
 	
 	if Input.is_action_just_pressed("open_fish_index"):
@@ -42,6 +52,7 @@ func _process(delta):
 			self.visible = false
 			is_fish_index_open = false
 			get_tree().paused = false
+			show_no_fish_selected_text()
 		else:
 			self.visible = true
 			is_fish_index_open = true
@@ -111,6 +122,178 @@ func _process(delta):
 	else:
 		striped_tigerbarb_fish_frame.get_node("Sprite2D").texture = preload("res://fishing/fish/fish_textures/unknown_fish.png")
 		striped_tigerbarb_fish_frame.get_node("Label").visible = true
+		
+
+func show_no_fish_selected_text():
+	detailed_fish_info_panel.visible = false
+	no_fish_selected_text.visible = true
+
+func hide_no_fish_selected_text():
+	detailed_fish_info_panel.visible = true
+	no_fish_selected_text.visible = false
+
+func fish_not_discovered():
+	detailed_fish_sprite.texture = preload("res://fishing/fish/fish_textures/unknown_fish.png")
+	detailed_fish_name.text = "???"
+	detailed_fish_rarity.text = "(??)"
+	detailed_fish_description.text = "Fish not discovered"
+
+
+func _on_close_shop_panel_button_pressed():
+	self.visible = false
+	get_tree().paused = false
+
+
+func _on_gray_fish_detailed_button_pressed():
+	hide_no_fish_selected_text()
+	
+	var fish_resource: Resource = preload("res://Inventory/items/gray_fish.tres")
+	
+	if player_has_gray_fish == false:
+		fish_not_discovered()
+		return
+	
+	detailed_fish_sprite.texture = fish_resource.texture
+	detailed_fish_name.text = fish_resource.name
+	detailed_fish_rarity.text = "(" + str(fish_resource.rarity) + ")"
+	detailed_fish_rarity.self_modulate = fish_resource.rarity_color
+	detailed_fish_description.text = fish_resource.description
+	detailed_fish_value.text = str(fish_resource.value)
+		
+func _on_clown_fish_detailed_button_pressed():
+	hide_no_fish_selected_text()
+	
+	var fish_resource: Resource = preload("res://Inventory/items/clown_fish.tres")
+	
+	if player_has_clown_fish == false:
+		fish_not_discovered()
+		return
+	
+	detailed_fish_sprite.texture = fish_resource.texture
+	detailed_fish_name.text = fish_resource.name
+	detailed_fish_rarity.text = "(" + str(fish_resource.rarity) + ")"
+	detailed_fish_rarity.self_modulate = fish_resource.rarity_color
+	detailed_fish_description.text = fish_resource.description
+	detailed_fish_value.text = str(fish_resource.value)
+
+
+func _on_blue_fish_detailed_button_pressed():
+	hide_no_fish_selected_text()
+	
+	var fish_resource: Resource = preload("res://Inventory/items/blue_fish.tres")
+	
+	if player_has_blue_fish == false:
+		fish_not_discovered()
+		return
+	
+	detailed_fish_sprite.texture = fish_resource.texture
+	detailed_fish_name.text = fish_resource.name
+	detailed_fish_rarity.text = "(" + str(fish_resource.rarity) + ")"
+	detailed_fish_rarity.self_modulate = fish_resource.rarity_color
+	detailed_fish_description.text = fish_resource.description
+	detailed_fish_value.text = str(fish_resource.value)
+
+func _on_yellow_tail_king_fish_detailed_button_pressed():
+	hide_no_fish_selected_text()
+	
+	var fish_resource: Resource = preload("res://Inventory/items/yellowtail_kingfish.tres")
+	
+	if player_has_yellowtail_kingfish == false:
+		fish_not_discovered()
+		return
+	
+	detailed_fish_sprite.texture = fish_resource.texture
+	detailed_fish_name.text = fish_resource.name
+	detailed_fish_rarity.text = "(" + str(fish_resource.rarity) + ")"
+	detailed_fish_rarity.self_modulate = fish_resource.rarity_color
+	detailed_fish_description.text = fish_resource.description
+	detailed_fish_value.text = str(fish_resource.value)
+
+func _on_green_widow_fish_detailed_button_pressed():
+	hide_no_fish_selected_text()
+	
+	var fish_resource: Resource = preload("res://Inventory/items/green_widow_fish.tres")
+	
+	if player_has_green_widow_fish == false:
+		fish_not_discovered()
+		return
+	
+	detailed_fish_sprite.texture = fish_resource.texture
+	detailed_fish_name.text = fish_resource.name
+	detailed_fish_rarity.text = "(" + str(fish_resource.rarity) + ")"
+	detailed_fish_rarity.self_modulate = fish_resource.rarity_color
+	detailed_fish_description.text = fish_resource.description
+	detailed_fish_value.text = str(fish_resource.value)
+
+
+func _on_brown_fish_detailed_button_pressed():
+	hide_no_fish_selected_text()
+	
+	var fish_resource: Resource = preload("res://Inventory/items/brown_fish.tres")
+	
+	if player_has_brown_fish == false:
+		fish_not_discovered()
+		return
+	
+	detailed_fish_sprite.texture = fish_resource.texture
+	detailed_fish_name.text = fish_resource.name
+	detailed_fish_rarity.text = "(" + str(fish_resource.rarity) + ")"
+	detailed_fish_rarity.self_modulate = fish_resource.rarity_color
+	detailed_fish_description.text = fish_resource.description
+	detailed_fish_value.text = str(fish_resource.value)
+
+
+func _on_blue_tang_fish_detailed_button_pressed():
+	hide_no_fish_selected_text()
+	
+	var fish_resource: Resource = preload("res://Inventory/items/blue_tang_fish.tres")
+	
+	if player_has_blue_tang_fish == false:
+		fish_not_discovered()
+		return
+	
+	detailed_fish_sprite.texture = fish_resource.texture
+	detailed_fish_name.text = fish_resource.name
+	detailed_fish_rarity.text = "(" + str(fish_resource.rarity) + ")"
+	detailed_fish_rarity.self_modulate = fish_resource.rarity_color
+	detailed_fish_description.text = fish_resource.description
+	detailed_fish_value.text = str(fish_resource.value)
+
+
+func _on_shiny_fish_detailed_button_pressed():
+	hide_no_fish_selected_text()
+	
+	var fish_resource: Resource = preload("res://Inventory/items/shiny_fish.tres")
+	
+	if player_has_shiny_fish == false:
+		fish_not_discovered()
+		return
+	
+	detailed_fish_sprite.texture = fish_resource.texture
+	detailed_fish_name.text = fish_resource.name
+	detailed_fish_rarity.text = "(" + str(fish_resource.rarity) + ")"
+	detailed_fish_rarity.self_modulate = fish_resource.rarity_color
+	detailed_fish_description.text = fish_resource.description
+	detailed_fish_value.text = str(fish_resource.value)
+
+
+func _on_striped_tiger_barb_fish_detailed_button_pressed():
+	hide_no_fish_selected_text()
+	
+	var fish_resource: Resource = preload("res://Inventory/items/striped_tigerbarb_fish.tres")
+	
+	if player_has_striped_tigerbarb_fish == false:
+		fish_not_discovered()
+		return
+	
+	detailed_fish_sprite.texture = fish_resource.texture
+	detailed_fish_name.text = fish_resource.name
+	detailed_fish_rarity.text = "(" + str(fish_resource.rarity) + ")"
+	detailed_fish_rarity.self_modulate = fish_resource.rarity_color
+	detailed_fish_description.text = fish_resource.description
+	detailed_fish_value.text = str(fish_resource.value)
+
+
 
 func save_data(save: PlayerData):
 	save.fish_types_the_player_has = fish_types_the_player_has
@@ -148,9 +331,3 @@ func load_data(save: PlayerData):
 	player_has_blue_tang_fish = save.player_has_blue_tang_fish
 	player_has_shiny_fish = save.player_has_shiny_fish
 	player_has_striped_tigerbarb_fish = save.player_has_striped_tigerbarb_fish
-
-
-
-func _on_close_shop_panel_button_pressed():
-	self.visible = false
-	get_tree().paused = false
